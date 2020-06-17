986aadd9608047bf034c5ffc6c4b63e7dd9a6288
gamonpo <gamonpo@outlook.com>
2020-06-17 09:08:54 -0300

    Desabilitar yellow box

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { StatusBar } from 'react-native';
 import Route from '~/routes';
 
 export default function App() {
+  console.disableYellowBox = true;
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
